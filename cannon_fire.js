23168631c9ba22682bbc52d3875a235c52329d8b
SgtGhatak <rahulgatak@hotmail.com>
2023-10-26 14:50:21 +1100

    Added cannon_fire.js
    Added the cannon_fire.js macro and two assets used for the macros

---
+++ cannon_fire.js
@@ -0,0 +1,49 @@
+// Author: Sgt_Ghatak
+// Date: 2023-10-26
+
+// Warpgate Crosshair
+let config = {
+    size: 1,
+    icon: 'icons/magic/fire/blast-jet-stream-splash.webp',
+    label: 'cannon fire',
+    tag: 'fire',
+    drawIcon: true,
+    drawOutline: true,
+    interval: 0
+}
+
+let position = await warpgate.crosshairs.show(config)
+
+new Sequence()
+
+    // Smoke 1 animation (long)
+    .effect()
+        .file("animated-spell-effects-cartoon.smoke.49")
+        .atLocation(position, {offset: {x:1.25}, gridUnits: true})
+        .size(3, {gridUnits: true})
+        .rotate(270) // change this value to shoot in a different direction
+        .zIndex(2)
+
+    // Smoke 2 animation (wide)
+    .effect()
+        .file("animated-spell-effects-cartoon.smoke.53")
+        .atLocation(position, {offset: {x:1.25}, gridUnits: true})
+        .size(3, {gridUnits: true})
+        .rotate(270) // change this value to shoot in a different direction
+        .zIndex(1)
+
+    // Cannonball animation
+    .effect()
+        .file("animated-spell-effects-cartoon.smoke.121")
+        .atLocation(position, {offset: {x:4}, gridUnits: true})
+        .rotate(0) // change this value to shoot in a different direction
+        .playbackRate(3)
+        .zIndex(0)
+
+    // Cannon sound effect
+    .sound()
+        .file("modules/vilandris-resources/Audio/Sound%20Effects/cannon-fire.mp3")
+        .volume(0.1)
+        .startTime(200)
+
+    .play()
